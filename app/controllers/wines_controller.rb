85051ba762c35baad1b7f46d9069757c6c00f596
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-04-16 13:38:00 -0700

    Added wines index view and table to show all wines.

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -2,6 +2,7 @@ class WinesController < ApplicationController
   before_action :find_wine, only: [:show, :edit, :update, :destroy]
 
   def index
+    @wines = Wine.all
   end
 
   def show
@@ -15,12 +16,19 @@ class WinesController < ApplicationController
 
   def create
     @wine = Wine.new(wine_params)
+
+    if @wine.save
+      redirect_to @wine, notice: "Successfully added a new wine!"
+    else
+      render 'new'
+    end
   end
 
   private
 
   def wine_params
     params.require(:wine).permit(:name, :vintage)
+  end
 
   def find_wine
     @wine = Wine.find(params[:id])
