805e239e3523986f33ad7de1ac827cc0dadebe5a
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-04-19 13:00:56 -0700

    added ratings attribute to new wine form

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -22,9 +22,16 @@ class WinesController < ApplicationController
   end
 
   def create
-    @wine = Wine.create(wine_params)
+
+    @wine = Wine.create(wine_params(:name, :vintage))
     @wine.users << current_user unless @wine.users.include?(current_user)
     binding.pry
+    @rating = Rating.new(star: wine_params(:ratings)[:ratings])
+    @rating.wine_id = @wine.id
+    @rating.user_id = current_user.id
+    @rating.save
+    @wine.ratings << @rating
+
 
     if @wine.save
       redirect_to @wine, notice: "Successfully added a new wine!"
@@ -35,8 +42,10 @@ class WinesController < ApplicationController
 
   private
 
-  def wine_params
-    params.require(:wine).permit(:name, :vintage)
+  def wine_params(*args)
+    params.require(:wine).permit(*args)
+    #params.require(:wine).permit(:name, :vintage, :rating)
+
   end
 
   def find_wine
