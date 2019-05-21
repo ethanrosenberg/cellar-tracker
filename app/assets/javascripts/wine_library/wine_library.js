bd8433b52be27d34773c7ff29056d25f7656e979
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-05-21 00:45:58 -0700

    Added json ams backend to the wine library.

--- app/assets/javascripts/wine_library/wine_library.js
+++ app/assets/javascripts/wine_library/wine_library.js
@@ -1,28 +1,49 @@
-Wine = function(id, name, vintage, user_count, star_rating) {
+Wine = function(id, name, vintage, rating) {
   this.id = id;
   this.name = name;
   this.vintage = vintage;
-  this.user_count = user_count;
-  this.star_rating = star_rating;
+  this.ratings = rating;
 
 };
 
 
 $(function() {
   //id = $('.wineInfo').data('id');
-  $.getJSON('/wines/' + id + '.json', function(data) {
+  $.getJSON('/wines/library.json', function(data) {
 
-    var wineText;
-    wine = new Wine(data.id, data.name, data.vintage, data.ratings[0]["star"], data.users_wines[0]["purchase_date"])
-    wineText = '<th scope="row">' + wine.id + '</th>';
-    wineText += '<td>' + wine.name+ '</td>';
-    wineText += '<td>' + wine.vintage + '</td>';
-    wineText += '<td>' + wine.rating + '</td>';
-    wineText += '<td>' + wine.niceDate() + '</td>';
+    data.forEach(row => {
+      //console.log(f)
+
+      let wineText;
+      wine = new Wine(row.id, row.name, row.vintage, row.ratings)
+      console.log(wine)
+
+      wineText = '<tr>';
+      wineText += '<th scope="row">' + wine.id + '</th>';
+      wineText += '<td>' + wine.name+ '</td>';
+      wineText += '<td>' + wine.vintage + '</td>';
+
+      let all_ratings = []
+      if (typeof wine.rating !== undefined ) {
+        wine.rating.forEach(rating => {
+          all_ratings.push(rating.star + ", ")
+        });
+
+      }
+
+      wineText += '<td>' + all_ratings+ '</td>';
+      wineText += '</tr>';
 
 
-    console.log(data)
-    return $('.wineInfo').prepend(wineText);
+
+      //console.log(data)
+    $('tbody').append(wineText);
+
+
+    });
+    //console.log(data)
+
+
     //return $(wineText).insertAfter( ".wineInfo" );
     //.insertAfter
   });
@@ -36,6 +57,21 @@ Wine.prototype.niceDate = function() {
   //d = Date.parse(this.purchase_date)
   console.log(this.purchase_date)
   return moment(this.purchase_date).format('ll');
+  //var currentDate = new Date(this.purchase_date);
+  //return Date.parse(this.purchase_date).toLocaleDateString();
+  //var date = currentDate.getDate();
+  //var month = currentDate.getMonth(); //Be careful! January is 0 not 1
+  //var year = currentDate.getFullYear();
+  //var dateString = date + "-" +(month + 1) + "-" + year;
+  //return currentDate.to_s;
+};
+
+Wine.prototype.ratingString = function() {
+  //console.log(this.purchase_date)
+  //d = Date.parse(this.purchase_date)
+return typeof this.wine.ratings == undefined ? "0 Ratings." : this.wine.ratings;
+
+  //return this.data.ratings.join(" ,");
   //var currentDate = new Date(this.purchase_date);
   //return Date.parse(this.purchase_date).toLocaleDateString();
   //var date = currentDate.getDate();
